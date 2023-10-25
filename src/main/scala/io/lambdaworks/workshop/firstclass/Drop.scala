3bbf89c031af281a78d625db5b54db3d1c92b3b8
vanjaftn <vanjateodorovic00@gmail.com>
2023-10-25 13:53:56 +0200

    Complete first-class function exercise

--- src/main/scala/io/lambdaworks/workshop/firstclass/Drop.scala
+++ src/main/scala/io/lambdaworks/workshop/firstclass/Drop.scala
@@ -6,6 +6,12 @@ object Drop {
     * Implement function that should drop elements that satisfy predicate function(p)
     * Implementation should pass suite DropFCFSpec
     */
-  def dropIf[A](elements: List[A], p: A => Boolean): List[A] = ???
+  def dropIf[A](elements: List[A], p: A => Boolean): List[A] = {
+      if (elements.length <= 1) {
+        elements
+      } else {
+        elements.filterNot(p(_))
+      }
+  }
 
 }
