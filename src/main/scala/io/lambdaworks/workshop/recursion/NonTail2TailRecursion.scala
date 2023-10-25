738e853108a860e3b6b056029b0bbe5c38c6c830
vanjaftn <vanjateodorovic00@gmail.com>
2023-10-25 17:01:38 +0200

    Implement changes in @tailrec functions

--- src/main/scala/io/lambdaworks/workshop/recursion/NonTail2TailRecursion.scala
+++ src/main/scala/io/lambdaworks/workshop/recursion/NonTail2TailRecursion.scala
@@ -11,7 +11,7 @@ object NonTail2TailRecursion {
   def factorial(n: Int): Int = {
     @tailrec
     def loop(n: Int, fact: Int = 1): Int =
-      if (n <= 0) 1 else loop(n - 1, fact * n)
+      if (n <= 0) fact else loop(n - 1, fact * n)
 
     loop(n)
   }
@@ -22,9 +22,10 @@ object NonTail2TailRecursion {
     def loop(cubedNumbers: List[Double] = List[Double]()): List[Double] = {
 
       numbers match {
-        case x :: xs if x % 2 == 0 => loop(Math.pow(x, 3) + cubedNumbers)
-        case _ :: xs => cubedNumbers
+        case x :: _ if x % 2 == 0 => loop(Math.pow(x, 3) +: cubedNumbers)
+        case _ :: _ => loop(cubedNumbers)
         case Nil     => List.empty
+        case _ => cubedNumbers
       }
 
     }
